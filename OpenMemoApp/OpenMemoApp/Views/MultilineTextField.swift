e1196f81b4c107fb4ae286cd47df11d565b87326
Ethan <Ethan@vincentdeMac-mini-2.local>
2026-08-11 17:04:14 +0800

    输入框：光标下移几像素，与 placeholder 垂直对齐

--- OpenMemoApp/OpenMemoApp/Views/MultilineTextField.swift
+++ OpenMemoApp/OpenMemoApp/Views/MultilineTextField.swift
@@ -19,7 +19,7 @@ struct MultilineTextField: UIViewRepresentable {
         tv.font = .preferredFont(forTextStyle: .body)
         tv.backgroundColor = .clear
         tv.isScrollEnabled = true
-        tv.textContainerInset = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 0)
+        tv.textContainerInset = UIEdgeInsets(top: 10, left: 0, bottom: 2, right: 0)
         tv.delegate = context.coordinator
         tv.returnKeyType = .send
         tv.onEnter = { onEnter() }
